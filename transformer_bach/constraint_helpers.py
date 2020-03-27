6e7c22d8b9be00c2826d1461b7208da9417995f1
asdfang <alex.s.fang@gmail.com>
2020-03-27 12:54:27 -0500

    add methods to convert music21 score to hold representation for constraining the melody

---
+++ transformer_bach/constraint_helpers.py
@@ -0,0 +1,43 @@
+# move to another file? chorale_dataset.py?
+
+import music21
+from transformer_bach.DatasetManager.helpers import SLUR_SYMBOL, standard_name
+
+
+def _has_one_part(stream):
+    """
+    Arguments
+        stream: music21.stream.Stream
+
+    returns True if input has only one part.
+    """
+    return stream.hasPartLikeStreams() == False or len(stream.parts) == 1
+
+
+def single_part_to_hold_representation(part):
+    """
+    Arguments
+        part: music21.stream.Stream – must contain only one line/part/voice
+
+    returns a list that represents the part in hold representation
+    """
+    assert _has_one_part(part)
+    hold_representation = []
+    part = part.flat.notesAndRests
+    for note in part:
+        hold_representation.append(standard_name(note))
+        num_holds = int(note.duration.quarterLength / 0.25 - 1.0)
+        hold_representation.extend([SLUR_SYMBOL] * num_holds)
+
+    return hold_representation
+
+
+def score_to_hold_representation_for_voice(score, voice=0):
+    """
+    Arguments
+        score: music21.stream.Score – probably a Bach chorale.
+        voice: which voice index to convert to hold representation; soprano by default.
+
+    returns a list that represents the specified part given by the voice index in hold representation
+    """
+    return single_part_to_hold_representation(score.parts[voice])
