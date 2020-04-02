cf801691d87b23019ef3a51a528bddd14322b85b
alisawuffles <alisa.46@gmail.com>
2020-04-02 01:07:26 -0500

    first commit

---
+++ transformer_bach/small_bach_dataloader.py
@@ -0,0 +1,68 @@
+from transformer_bach.DatasetManager.chorale_dataset import ChoraleBeatsDataset
+from transformer_bach.DatasetManager.dataset_manager import DatasetManager
+from transformer_bach.DatasetManager.metadata import FermataMetadata, TickMetadata, KeyMetadata
+from transformer_bach.DatasetManager.helpers import NextChoralesIteratorGen
+from transformer_bach.dataloader_generator import DataloaderGenerator
+
+subdivision = 4
+num_voices = 4
+metadatas = [
+    FermataMetadata(),
+    TickMetadata(subdivision=subdivision),
+    KeyMetadata()
+]
+include_transpositions = True
+
+
+class SmallBachDataloaderGenerator(DataloaderGenerator):
+    def __init__(self, sequences_size, start_idx, end_idx):
+        dataset_manager = DatasetManager()
+
+        chorale_dataset_kwargs = {
+            'voice_ids':      [0, 1, 2, 3],
+            'metadatas':      metadatas,
+            'sequences_size': sequences_size,
+            'subdivision':    subdivision,
+            'include_transpositions':   include_transpositions,
+        }
+
+        dataset_manager.add_dataset(
+            dataset_name=f'bach_chorales_beats_{start_idx}_to_{end_idx}',
+            dataset_class_name=ChoraleBeatsDataset,
+            corpus_it_gen=NextChoralesIteratorGen(start_idx, end_idx)
+        )
+
+        dataset: ChoraleBeatsDataset = dataset_manager.get_dataset(
+            name=f'bach_chorales_beats_{start_idx}_to_{end_idx}',
+            **chorale_dataset_kwargs
+        )
+
+        super(SmallBachDataloaderGenerator, self).__init__(dataset=dataset)
+
+    def dataloaders(self, batch_size, num_workers=0, shuffle_train=True,
+                    shuffle_val=False):
+        # discard metadata
+        # and put num_channels (num_voices) at the last dimension
+        return [({'x': t[0].transpose(1, 2)}
+                 for t in dataloader)
+                for dataloader
+                in self.dataset.data_loaders(batch_size, num_workers=num_workers,
+                                             split=[1, 0],
+                                             shuffle_train=shuffle_train,
+                                             shuffle_val=shuffle_val
+                                             )]
+
+    def write(self, x, path):
+        """
+
+        :param x: (batch_size, num_events, num_channels)
+        :return: list of music21.Score
+        """
+        score = self.dataset.tensor_to_score(x.transpose(1, 0))
+        score.write('xml', f'{path}.xml')
+        return
+
+    def to_score(self, tensor_scores):
+        scores = [self.dataset.tensor_to_score(tensor_score.transpose(1, 0))
+                  for tensor_score in tensor_scores]
+        return scores
